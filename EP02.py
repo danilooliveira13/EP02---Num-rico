72736811da869ae7683c2c0c09ba12b16b47587f
danilooliveira13 <69092987+danilooliveira13@users.noreply.github.com>
2022-06-03 20:27:12 -0300

    Add files via upload

---
+++ EP02.py
@@ -0,0 +1,10 @@
+#Autores: Danilo Oliveira da Silva, 11848080
+#         Renato Naves Fleury
+
+from cmath import pi
+from string import digits
+import numpy as np
+import math
+
+def main():
+    a = 
