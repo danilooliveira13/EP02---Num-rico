721aee71c6b8d2cb572a5f28752487376f9a59e3
Renato Naves Fleury <93680666+RenatoFleury@users.noreply.github.com>
2022-06-05 03:49:25 -0300

    Update EP02.py

--- EP02.py
+++ EP02.py
@@ -5,11 +5,11 @@ import numpy as np
 import math
 
 # W6 W8 W10
-W = [[0],
-     [2],
-     [1],
-     [0.8888888888888888888888889, 0.5555555555555555555555556],
-     [0.6521451548625461426269360, 0.3478548451374538573730639],
+W_d = [[],
+     [],
+     [],
+     [],
+     [],
      [],
      [0.4679139345726910473898703, 0.3607615730481386075698335, 0.1713244923791703450402961],
      [],
@@ -17,11 +17,11 @@ W = [[0],
      [],
      [0.2955242247147528701738930, 0.2692667193099963550912269, 0.2190863625159820439955349, 0.1494513491505805931457763, 0.0666713443086881375935688]]
 # X6 X8 X10
-X = [[0],
-     [0],
-     [0.5773502691896257645091487],
-     [0, 0.7745966692414833770358531],
-     [0.3399810435848562648026658, 0.8611363115940525752239465],
+X_d = [[],
+     [],
+     [],
+     [],
+     [],
      [],
      [0.2386191860831969086305017, 0.6612093864662645136613996, 0.9324695142031520278123016],
      [],
@@ -31,6 +31,10 @@ X = [[0],
 
 # Integral Numérica de Gauss: f(x)dx em [-1,1]
 def integral_gauss_1A(f, n):
+    X = np.array(X_d[n])
+    W = np.array(W_d[n])
+    X = np.concatenate((-X[::-1], X))
+    W = np.concatenate((W[::-1], W))
     somatorio = 0
     # Quantidade par de nós:
     if (n%2 == 0):
@@ -52,21 +56,15 @@ def integral_gauss_1B(f, a, b, n):
     return integral_gauss_1A(g, n)
 
 # Integral Numérica de Gauss: f(x,y)dydx em [-1,1] e [-1,1]
-def integral_gauss_2A(f, n): 
+def integral_gauss_2A(f, n):
+    X = np.array(X_d[n])
+    W = np.array(W_d[n])
+    X = np.concatenate((-X[::-1], X))
+    W = np.concatenate((W[::-1], W))
     somatorio = 0
-    # n par:
-    if (n%2 == 0):
-        for i in range(0, round(n/2)):
-            for j in range(0, round(n/2)):
-                somatorio += (W[n][i]*W[n][j] * (f(-X[n][i],-X[n][j]) + f(X[n][i],X[n][j])))
-    # n ímpar:
-    else:
-        for i in range(0, round(n/2)):
-            for j in range(0, round(n/2)):
-                if (j == 0):
-                    somatorio += W[n][i]*W[n][j] * f(X[n][i], X[n][j])
-                else:
-                    somatorio += (W[n][i]*W[n][j] * (f(-X[n][i],-X[n][j]) + f(X[n][i],X[n][j])))
+    for i in range(n):
+        for j in range(n):
+            somatorio += (W[i]*W[j] * f(X[i],X[j]))
     return somatorio 
 
 # Integral Numérica de Gauss: f(x,y)dydx em [a,b] e [c(x),d(x)]
@@ -78,23 +76,30 @@ def integral_gauss_2B(f, a, b, c, d , n):
     return integral_gauss_2A(g2, n)
 
 # Integral Numérica de Gauss: f(x,y,z)dzdydx em [-1,1], [-1,1] e [-1,1]
-def integral_gauss_3A(f, n): 
+def integral_gauss_3A(f, n):
+    X = np.array(X_d[n])
+    W = np.array(W_d[n])
+    X = np.concatenate((-X[::-1], X))
+    W = np.concatenate((W[::-1], W))
     somatorio = 0
-    # n par:
-    if (n%2 == 0):
-        for i in range(0, round(n/2)):
-            for j in range(0, round(n/2)):
-                for k in range(0, round(n/2)):
-                    somatorio += (W[n][i]*W[n][j]*W[n][k] * (f(-X[n][i],-X[n][j],-X[n][k]) + f(X[n][i],X[n][j],X[n][k])))
+    # if (n%2 == 0):
+        # for i in range(0, round(n/2)):
+        #     for j in range(0, round(n/2)):
+        #         for k in range(0, round(n/2)):
+        #             somatorio += (W[n][i]*W[n][j]*W[n][k] * (f(-X[n][i],-X[n][j],-X[n][k]) + f(X[n][i],X[n][j],X[n][k])))
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                somatorio += (W[i]*W[j]*W[k] * f(X[i],X[j],X[k]))
     # n ímpar:
-    else:
-        for i in range(0, round(n/2)):
-            for j in range(0, round(n/2)):
-                for k in range(0, round(n/2)):
-                    if (j == 0):
-                        somatorio += (W[n][i]*W[n][j]*W[n][k] * f(X[n][i],X[n][j],X[n][k]))
-                    else:
-                        somatorio += (W[n][i]*W[n][j]*W[n][k] * (f(-X[n][i],-X[n][j],-X[n][k]) + f(X[n][i],X[n][j],X[n][k])))
+    # else:
+    #     for i in range(0, round(n/2)):
+    #         for j in range(0, round(n/2)):
+    #             for k in range(0, round(n/2)):
+    #                 if (j == 0):
+    #                     somatorio += (W[n][i]*W[n][j]*W[n][k] * f(X[n][i],X[n][j],X[n][k]))
+    #                 else:
+    #                     somatorio += (W[n][i]*W[n][j]*W[n][k] * (f(-X[n][i],-X[n][j],-X[n][k]) + f(X[n][i],X[n][j],X[n][k])))
     return somatorio 
 
 # Integral Numérica de Gauss: f(x,y,z)dzdydx em [a,b], [c(x),d(x)] e [e(x,y),f(x,y)]
@@ -109,16 +114,42 @@ def integral_gauss_3B(f1, a, b, c, d, e, f, n):
 
 # Exemplo 1A: área do cubo
 def exemplo_1A():
-    def f(x,y,z):
+    def f1(x,y,z):
         return 1
-    resultados = np.array([])
+    def c(x):
+        return 0
+    def d(x):
+        return 1
+    def e(x,y):
+        return 0
+    def f(x,y):
+        return 1
+    resultados = []
     n = [6, 8, 10]
     for i in n:
-        resultados = np.append(integral_gauss_3B(f,0,1,0,1,0,1,i))
+        resultados.append(integral_gauss_3B(f1,0,1,c,d,e,f,i))
     print("\nExemplo 1A - Área do cubo de aresta 1:\n")
     for i in range(3):
         print('\tn = {}\tResultado = {:0.22f}'.format(n[i], resultados[i]))
 
 def main():
+    def f2(x,y):
+        return 1
+    a=0
+    b=1
+    def c(x):
+        return 0
+    def d(x):
+        return 1
+    resultados = []
+    n = [6, 8, 10]
+    for i in n:
+        resultados.append(integral_gauss_2B(f2,0,1,c,d,i))
+    print("\tTeste:")
+    for i in range(3):
+        print('\tn = {}\tResultado = {:0.22f}'.format(n[i], resultados[i]))
+
+    print('\n')
     exemplo_1A()
+
 main()
